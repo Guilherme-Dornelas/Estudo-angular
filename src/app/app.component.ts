68c05bb281181fb7d92e5c3ae7a4c331da5ba947
Guilherme Dornelas de Arruda <guilhermedornelas347@gmail.com>
2025-02-07 15:24:53 -0300

    Estudo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CarrosComponent } from "../componentes/carros/carros.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CarrosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
